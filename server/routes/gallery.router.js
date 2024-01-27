73a120b212b9200c18cf106b848f8945d442450e
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-27 15:26:05 -0500

    router file GET Route established.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Pool } = require('pg');
 const router = express.Router();
 
 // PUT /gallery/like/:id
@@ -9,6 +10,17 @@ router.put('/like/:id', (req, res) => {
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const dbQuery = `SELECT * FROM "gallery";`;
+  Pool
+    .query(dbQuery)
+    .then((result) => {
+      console.log('Server query successful')
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Server Query Failed: ', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
