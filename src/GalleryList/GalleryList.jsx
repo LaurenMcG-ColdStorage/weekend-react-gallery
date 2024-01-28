60773d644f64fe67584ccc9047df2729dd4dbbfd
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-28 11:08:05 -0500

    GalleryList set to take in prop, render GalleryItem for each value in prop

--- src/GalleryList/GalleryList.jsx
+++ src/GalleryList/GalleryList.jsx
@@ -1,7 +1,15 @@
 
 function GalleryList(gallery, setGalleryCallback){
+    console.log(gallery);
+    
     return(
-
+      <div>
+        {gallery.map(pic => {
+            return(
+                <GalleryItem key={pic.id} pic={pic}/>
+            )
+        })}
+      </div>  
     );
 };
 
