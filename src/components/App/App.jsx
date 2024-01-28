4c2e4ea9f25b362baeba4f79a695e40b8feccbb8
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-27 21:06:03 -0500

    Set up imports for App file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
+import react from 'react'
+import GalleryItem from '../../GalleryItem/GalleryItem';
+import GalleryList from '../../GalleryList/GalleryList';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [gallery, setGallery] = useState([]);
+
     return (
-      <div>
+      <div data-testid='app'>
         <header>
           <h1>React Gallery</h1>
         </header>
 
         <p>The gallery goes here!</p>
+        <GalleryList />
         <img src="images/goat_small.jpg"/>
       </div>
     );
